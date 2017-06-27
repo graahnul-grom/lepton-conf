3900f77d71fb80a95dafd89f277b1e630e7077d2
dmn <graahnul.grom@gmail.com>
2017-06-27 18:57:02 +0300

    load and display geda config

--- cfg_edit_dlg.c
+++ cfg_edit_dlg.c
@@ -1,4 +1,6 @@
 #include "cfg_edit_dlg.h"
+#include <liblepton/liblepton.h>
+#include <liblepton/libgedaguile.h>
 
 enum
 {
@@ -6,6 +8,10 @@ enum
     COL_2_VAL,
     NUM_COLS
 };
+
+
+
+static void load_cfg( cfg_edit_dlg* dlg );
 
 
 
@@ -127,8 +133,8 @@ set_property( GObject* obj, guint id, const GValue* val, GParamSpec* spec )
 static void
 dispose( GObject* obj )
 {
-    printf( "cfg_edit_dlg::dispose( %p ); refcnt: %d\n",
-        obj, obj ? obj->ref_count : 0 );
+//    printf( "cfg_edit_dlg::dispose( %p ); refcnt: %d\n",
+//        obj, obj ? obj->ref_count : 0 );
 
     cfg_edit_dlgClass* cls = CFG_EDIT_DLG_GET_CLASS( obj );
 
@@ -196,9 +202,10 @@ cfg_edit_dlg_init( cfg_edit_dlg* dlg )
     add_col( dlg->tree_v_, ren_text, "text",   colid_val(), "val" );
 
     // tree vire content:
-    GtkTreeIter it = add_row( dlg, "[name]", "[val]", NULL );
-    it = add_row( dlg, "123", "456", &it );
+//    GtkTreeIter it = add_row( dlg, "[name]", "[val]", NULL );
+//    it = add_row( dlg, "123", "456", &it );
 
+    load_cfg( dlg );
 
     // content area:
     //
@@ -231,4 +238,127 @@ cfg_edit_dlg_init( cfg_edit_dlg* dlg )
                       G_CALLBACK( &cfg_edit_dlg_on_btn_apply ),
                       NULL );
 }
+
+
+
+
+
+static void
+load_keys( EdaConfig*    ctx,
+           const gchar*  group,
+           cfg_edit_dlg* dlg,
+           GtkTreeIter*  itParent )
+{
+    gsize len = 0;
+    GError* err = NULL;
+
+    gchar** pp = eda_config_get_keys( ctx, group, &len, &err );
+    if ( pp == NULL )
+    {
+        printf( " >> load_keys(): !eda_config_get_keys()\n" );
+        if ( err != NULL )
+        {
+            printf( "    err: %s\n", err->message );
+        }
+
+        g_clear_error( &err );
+        return;
+    }
+
+    for ( gsize ndx = 0; ndx < len; ++ndx )
+    {
+        const gchar* name = pp[ ndx ];
+        printf( "    [%s]\n", name );
+
+        gchar* val = eda_config_get_string( ctx, group, name, &err );
+        if ( val == NULL )
+        {
+            printf( " >> load_keys( %s ): !eda_config_get_string( %s )\n",
+                    group, name );
+            if ( err != NULL )
+            {
+                printf( "    err: %s\n", err->message );
+            }
+
+            val = g_strdup( "[err]" );
+        }
+
+        add_row( dlg, name, val, itParent );
+
+        g_free( val );
+    }
+
+    g_strfreev( pp );
+
+} // load_keys()
+
+
+
+static void
+load_groups( EdaConfig*    ctx,
+             const gchar*  fname,
+             cfg_edit_dlg* dlg,
+             GtkTreeIter*  itParent )
+{
+    if ( fname != NULL )
+    {
+        GError* err = NULL;
+        gboolean res = eda_config_load( ctx, &err );
+        if ( !res )
+        {
+            printf( " >> load_groups(): !eda_config_load()\n" );
+
+            if ( err != NULL )
+            {
+                printf( "    err: %s\n", err->message );
+            }
+
+            g_clear_error( &err );
+            return;
+        }
+    }
+
+    gsize len = 0;
+    gchar** pp = eda_config_get_groups( ctx, &len );
+    if ( pp == NULL )
+    {
+        printf( " >> load_groups(): !eda_config_get_groups()\n" );
+        return;
+    }
+
+    printf( " >> num of grps: <%lu>\n", len );
+
+    for ( gsize ndx = 0; ndx < len; ++ndx )
+    {
+        printf( "  <%s>\n", pp[ndx] );
+        GtkTreeIter it = add_row( dlg, pp[ndx], "", itParent );
+
+        load_keys( ctx, pp[ndx], dlg, &it );
+    }
+
+    g_strfreev( pp );
+
+} // load_groups()
+
+
+
+static void
+load_cfg( cfg_edit_dlg* dlg )
+{
+    EdaConfig* ctx = NULL;
+//    ctx = eda_config_get_default_context();
+//    ctx = eda_config_get_system_context();
+    ctx = eda_config_get_user_context();
+//    ctx = eda_config_get_context_for_path( "." );
+
+    const gchar* fname = eda_config_get_filename( ctx );
+
+    GtkTreeIter it = add_row( dlg, "|| user ||", fname ? fname : "", NULL );
+
+    load_groups( ctx, fname, dlg, &it );
+}
+
+
+
+
 
