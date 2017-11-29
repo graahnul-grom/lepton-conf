e3334213685c5b1723a7984a879c1fb01f9b7935
dmn <graahnul.grom@gmail.com>
2017-11-29 06:22:25 +0300

    add dlg_add_val_run(); adding key/values works

--- cfg_edit_dlg.c
+++ cfg_edit_dlg.c
@@ -51,6 +51,9 @@ enum
 
 
 static gboolean
+cfg_edit_dlg_add_key_val( row_data* rdata, const gchar* key, const gchar* val );
+
+static gboolean
 cfg_edit_dlg_chg_val( row_data* rdata, const gchar* txt );
 
 static void
@@ -1005,27 +1008,31 @@ cfg_edit_dlg_init( cfg_edit_dlg* dlg )
 //
 
 static gchar*
-edit_val_dlg( cfg_edit_dlg* dlg, const gchar* txt, const gchar* title )
+dlg_edit_val_run( cfg_edit_dlg* dlg, const gchar* txt, const gchar* title )
 {
-    GtkWidget* evdlg = gtk_dialog_new_with_buttons(
-        title ? title : "Edit",
+    GtkWidget* vdlg = gtk_dialog_new_with_buttons(
+        title ? title : "Edit value:",
         GTK_WINDOW( dlg ),
         GTK_DIALOG_MODAL, // | GTK_DIALOG_DESTROY_WITH_PARENT,
         GTK_STOCK_OK,     GTK_RESPONSE_ACCEPT,
         GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT,
         NULL );
 
-    gtk_window_set_title( GTK_WINDOW( evdlg ), title );
+    if ( title )
+        gtk_window_set_title( GTK_WINDOW( vdlg ), title );
 
     GtkWidget* ent = gtk_entry_new();
     gtk_entry_set_text( GTK_ENTRY( ent ), txt );
 
-    GtkWidget* ca = gtk_dialog_get_content_area( GTK_DIALOG( evdlg ) );
-    gtk_box_pack_start( GTK_BOX( ca ), ent, TRUE, TRUE, 10 );
+    GtkWidget* vbox = gtk_vbox_new( TRUE, 5 );
+    gtk_box_pack_start( GTK_BOX( vbox ), ent, TRUE, TRUE, 5 );
 
-    gtk_widget_show_all( evdlg );
-    gtk_widget_set_size_request( evdlg, 300, -1 );
-    gint res = gtk_dialog_run( GTK_DIALOG( evdlg ) );
+    GtkWidget* ca = gtk_dialog_get_content_area( GTK_DIALOG( vdlg ) );
+    gtk_box_pack_start( GTK_BOX( ca ), vbox, TRUE, TRUE, 10 );
+
+    gtk_widget_show_all( vdlg );
+    gtk_widget_set_size_request( vdlg, 300, -1 );
+    gint res = gtk_dialog_run( GTK_DIALOG( vdlg ) );
 
     printf( "  edit_val_dlg(): resp: %d\n", res );
 
@@ -1036,11 +1043,64 @@ edit_val_dlg( cfg_edit_dlg* dlg, const gchar* txt, const gchar* title )
         ret = g_strdup( gtk_entry_get_text( GTK_ENTRY( ent ) ) );
     }
 
-    gtk_widget_destroy( evdlg );
+    gtk_widget_destroy( vdlg );
 
     return ret;
 
-} // edit_val_dlg()
+} // dlg_edit_val_run()
+
+
+
+static gboolean
+dlg_add_val_run( cfg_edit_dlg* dlg,
+                 const gchar* title,
+                 gchar** key,
+                 gchar** val )
+{
+    GtkWidget* vdlg = gtk_dialog_new_with_buttons(
+        title ? title : "Add key/value:",
+        GTK_WINDOW( dlg ),
+        GTK_DIALOG_MODAL,
+        GTK_STOCK_OK,     GTK_RESPONSE_ACCEPT,
+        GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT,
+        NULL );
+
+    if ( title )
+        gtk_window_set_title( GTK_WINDOW( vdlg ), title );
+
+    GtkWidget* ent_key = gtk_entry_new();
+    gtk_entry_set_text( GTK_ENTRY( ent_key ), "newKey" );
+
+    GtkWidget* ent_val = gtk_entry_new();
+    gtk_entry_set_text( GTK_ENTRY( ent_val ), "newVal" );
+
+    GtkWidget* vbox = gtk_vbox_new( TRUE, 5 );
+    gtk_box_pack_start( GTK_BOX( vbox ), ent_key, TRUE, TRUE, 5 );
+    gtk_box_pack_start( GTK_BOX( vbox ), ent_val, TRUE, TRUE, 5 );
+
+    GtkWidget* ca = gtk_dialog_get_content_area( GTK_DIALOG( vdlg ) );
+    gtk_box_pack_start( GTK_BOX( ca ), vbox, TRUE, TRUE, 10 );
+
+    gtk_widget_show_all( vdlg );
+    gtk_widget_set_size_request( vdlg, 300, -1 );
+    gint res = gtk_dialog_run( GTK_DIALOG( vdlg ) );
+
+    printf( "  edit_val_dlg(): resp: %d\n", res );
+
+    gboolean ret = FALSE;
+
+    if ( res == GTK_RESPONSE_ACCEPT )
+    {
+        *key = g_strdup( gtk_entry_get_text( GTK_ENTRY( ent_key ) ) );
+        *val = g_strdup( gtk_entry_get_text( GTK_ENTRY( ent_val ) ) );
+        ret = TRUE;
+    }
+
+    gtk_widget_destroy( vdlg );
+
+    return ret;
+
+} // dlg_add_val_run()
 
 
 
@@ -1061,7 +1121,7 @@ cfg_edit_dlg_on_mitem_edit( GtkMenuItem* mitem, gpointer p )
 
     printf( "cfg_edit_dlg_on_mitem_edit(): k: [%s], v: [%s]\n", rdata->key_, rdata->val_ );
 
-    gchar* txt = edit_val_dlg( dlg, rdata->val_, "Edit value:" );
+    gchar* txt = dlg_edit_val_run( dlg, rdata->val_, NULL );
 
     if ( txt && cfg_edit_dlg_chg_val( rdata, txt ) )
     {
@@ -1103,7 +1163,17 @@ cfg_edit_dlg_on_mitem_add( GtkMenuItem* mitem, gpointer p )
 
     printf( "cfg_edit_dlg_on_mitem_add(): g: [%s]\n", rdata->group_ );
 
-    gchar* txt = edit_val_dlg( dlg, "newval", "Add key/value" );
+    gchar* key = NULL;
+    gchar* val = NULL;
+    if ( dlg_add_val_run( dlg, NULL, &key, &val ) )
+    {
+        printf( "cfg_edit_dlg_on_mitem_add(): [%s] =>[%s]\n", key, val );
+
+        cfg_edit_dlg_add_key_val( rdata, key, val );
+
+        g_free( key );
+        g_free( val );
+    }
 
 } // cfg_edit_dlg_on_mitem_add()
 
@@ -1480,6 +1550,22 @@ ctx_get_fname( EdaConfig* ctx, gboolean* exist, gboolean* rok, gboolean* wok )
     return fname;
 }
 
+
+
+
+static gboolean
+cfg_edit_dlg_add_key_val( row_data* rdata, const gchar* key, const gchar* val )
+{
+    eda_config_set_string( rdata->ctx_,
+                           rdata->group_,
+                           key,
+                           val );
+
+    GError* err = NULL;
+    gboolean res = eda_config_save( rdata->ctx_, &err );
+
+    return res;
+}
 
 
 
