234fcb70c732914b1efbb1a7b05c1b5c11203d00
dmn <graahnul.grom@gmail.com>
2018-03-09 00:18:55 +0300

    use getopt() to parse cmd line

--- main.c
+++ main.c
@@ -24,6 +24,22 @@
 
 
 
+void out_version()
+{
+    printf( "lepton-conf version 1.0\n" );
+    exit( 0 );
+}
+
+
+
+void out_help( int exit_code )
+{
+    printf( "usage: lepton-conf [-h] [-v] [dirname]\n" );
+    exit( exit_code );
+}
+
+
+
 int main( int argc, char* argv[] )
 {
 //    scm_init_guile();
@@ -31,25 +47,59 @@ int main( int argc, char* argv[] )
 
     gtk_init( &argc, &argv );
 
+
+    int ch = -1;
+    do
+    {
+        int ch = getopt( argc, argv, "hvd:" );
+
+        if ( ch == 'v' )
+            out_version();
+        else
+        if ( ch == 'h' )
+            out_help( 0 );
+        else
+        if ( ch == '?' )
+            out_help( 1 );
+    }
+    while ( ch != -1 );
+
+    printf( " >> optind:         [ %d ]\n", optind );
+    printf( " >> argv[ optind ]: [ %s ]\n", argv[ optind ] );
+
+    const gchar* dir = argv[ optind ];
+    if ( dir != NULL )
+    {
+        chdir( dir );
+    }
+
+
+    /* w/o getopt():
+    *
     if ( argc >= 2 )
-        chdir( argv[ 1 ] );
+    {
+        const gchar* dir = NULL;
+        dir = argv[ 1 ];
+        chdir( dir );
+        printf( " >> CD: [ %s ]\n\n", dir );
+    }
+    */
+
 
     GtkWidget* wnd = cfg_edit_dlg_new();
 
 //    gtk_widget_set_size_request( wnd, 800, 650 );
 //    gtk_window_set_default_size( GTK_WINDOW(wnd), 800, 1000 );
-
-
 //    gtk_dialog_run( GTK_DIALOG( wnd ) );
 
+    g_signal_connect( G_OBJECT( wnd ),
+                      "destroy",
+                      G_CALLBACK( &gtk_main_quit ),
+                      NULL );
 
-        g_signal_connect( G_OBJECT( wnd ),
-                          "destroy",
-                          G_CALLBACK( &gtk_main_quit ),
-                          NULL );
+    gtk_widget_show_all( wnd );
 
-        gtk_widget_show_all( wnd );
-        gtk_main();
+    gtk_main();
 
     printf( " -- -- -- main(): done\n\n");
     return 0;
