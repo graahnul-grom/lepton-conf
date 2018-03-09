dfb846ce9a9189b3d18416846f4518a014100d11
dmn <graahnul.grom@gmail.com>
2018-03-09 21:31:06 +0300

    [R]: setup signals handlers in separate func

--- cfg_edit_dlg.c
+++ cfg_edit_dlg.c
@@ -2532,6 +2532,70 @@ conf_save( EdaConfig* ctx, cfg_edit_dlg* dlg )
 *
 */
 
+// setup signal handlers for dialog:
+//
+static void
+gui_mk_events( cfg_edit_dlg* dlg )
+{
+    g_signal_connect( G_OBJECT( dlg ),
+                      "delete-event",
+                      G_CALLBACK( &on_delete_event ),
+                      NULL );
+
+    g_signal_connect( G_OBJECT( dlg->btn_showinh_ ),
+                      "toggled",
+                      G_CALLBACK( &on_btn_showinh ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->lab_fname_ ),
+                      "activate-link",             // mouse click
+                      G_CALLBACK( &on_lab_fname ),
+                      dlg );
+    g_signal_connect( G_OBJECT( dlg->lab_fname_ ),
+                      "activate-current-link",     // press Enter key
+                      G_CALLBACK( &on_lab_fname ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->btn_reload_ ),
+                      "clicked",
+                      G_CALLBACK( &on_btn_reload ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->btn_tst_ ),
+                      "clicked",
+                      G_CALLBACK( &on_btn_tst ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->btn_add_ ),
+                      "clicked",
+                      G_CALLBACK( &on_btn_add ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->btn_edit_ ),
+                      "clicked",
+                      G_CALLBACK( &on_btn_edit ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->btn_toggle_ ),
+                      "clicked",
+                      G_CALLBACK( &on_btn_toggle ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->tree_v_ ),
+                      "cursor-changed",         // tree sel changed
+//                      "row-activated",
+                      G_CALLBACK( &on_row_sel ),
+                      dlg );
+
+    g_signal_connect( G_OBJECT( dlg->tree_v_ ),
+                      "button-press-event",
+                      G_CALLBACK( &on_mouse_click ),
+                      dlg );
+
+} // gui_mk_events()
+
+
+
 static void
 gui_mk_labels_line( const gchar* left_txt,
                 GtkWidget* right_label,
@@ -2774,60 +2838,7 @@ gui_mk( cfg_edit_dlg* dlg, const gchar* cwd )
 
     // event handlers:
     //
-    g_signal_connect( G_OBJECT( dlg ),
-                      "delete-event",
-                      G_CALLBACK( &on_delete_event ),
-                      NULL );
-
-    g_signal_connect( G_OBJECT( dlg->btn_showinh_ ),
-                      "toggled",
-                      G_CALLBACK( &on_btn_showinh ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->lab_fname_ ),
-                      "activate-link",             // mouse click
-                      G_CALLBACK( &on_lab_fname ),
-                      dlg );
-    g_signal_connect( G_OBJECT( dlg->lab_fname_ ),
-                      "activate-current-link",     // press Enter key
-                      G_CALLBACK( &on_lab_fname ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->btn_reload_ ),
-                      "clicked",
-                      G_CALLBACK( &on_btn_reload ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->btn_tst_ ),
-                      "clicked",
-                      G_CALLBACK( &on_btn_tst ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->btn_add_ ),
-                      "clicked",
-                      G_CALLBACK( &on_btn_add ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->btn_edit_ ),
-                      "clicked",
-                      G_CALLBACK( &on_btn_edit ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->btn_toggle_ ),
-                      "clicked",
-                      G_CALLBACK( &on_btn_toggle ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->tree_v_ ),
-                      "cursor-changed",
-//                      "row-activated",
-                      G_CALLBACK( &on_row_sel ),
-                      dlg );
-
-    g_signal_connect( G_OBJECT( dlg->tree_v_ ),
-                      "button-press-event",
-                      G_CALLBACK( &on_mouse_click ),
-                      dlg );
+    gui_mk_events( dlg );
 
 
     //
