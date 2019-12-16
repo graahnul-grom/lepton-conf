90d419a9a1a01822a5cca8b296d23ef45e7cd260
dmn <graahnul.grom@gmail.com>
2019-12-16 12:05:58 +0300

    sort: a_chg_val(). TODO...
    if sort model is used, row_field_get_data() after
    row_field_set_val() causes crash.
    see comments

--- actions.c
+++ actions.c
@@ -31,6 +31,22 @@ a_chg_val( cfg_edit_dlg*   dlg,
     {
         row_field_set_val( dlg, it, txt );
 
+
+
+
+        // XXX:  fixes crash in subsequent calls to row_field_get_data()
+        // NOTE: crashes only if sort model is used
+        // TODO: does gtk_tree_store_set() invalidate iterators??
+        //
+        row_cur_get_iter( dlg, &it );
+        //
+        //
+        //
+        //
+
+
+
+
         // unset inherited:
         //
         row_key_unset_inh( dlg, it );
