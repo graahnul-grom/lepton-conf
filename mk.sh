bee9f04b01dc2007cd89933af902112b2b19c2a9
dmn <graahnul.grom@gmail.com>
2021-03-11 05:59:09 +0300

    mk.sh, run.sh: do not use guile 3

--- mk.sh
+++ mk.sh
@@ -2,9 +2,9 @@
 
 export GU3=/tmp/DMN/gu3
 
-export PKG_CONFIG_PATH=${GU3}/lib/pkgconfig:$lepton/bin.master/lib/pkgconfig
+# export PKG_CONFIG_PATH=${GU3}/lib/pkgconfig:$lepton/bin.master/lib/pkgconfig
 # export PKG_CONFIG_PATH=$lepton/bin.master/lib/pkgconfig
-# export PKG_CONFIG_PATH=$lepton/bin.tb/lib/pkgconfig
+export PKG_CONFIG_PATH=$lepton/bin.tb/lib/pkgconfig
 # make clean
 make -j4 DBG="-O0 -ggdb -DDEBUG" "$@"
 # make -j4 "$@"
