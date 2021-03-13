452842bab6de192df6563ac308674bcc401db2ed
dmn <graahnul.grom@gmail.com>
2021-03-13 06:13:18 +0300

    build with lepton master

--- mk.sh
+++ mk.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-export GU3=/tmp/DMN/gu3
-
+# export GU3=/tmp/DMN/gu3
 # export PKG_CONFIG_PATH=${GU3}/lib/pkgconfig:$lepton/bin.master/lib/pkgconfig
-# export PKG_CONFIG_PATH=$lepton/bin.master/lib/pkgconfig
-export PKG_CONFIG_PATH=$lepton/bin.tb/lib/pkgconfig
+
+export PKG_CONFIG_PATH=$lepton/bin.master/lib/pkgconfig
+# export PKG_CONFIG_PATH=$lepton/bin.tb/lib/pkgconfig
+
 # make clean
+
 make -j4 DBG="-O0 -ggdb -DDEBUG" "$@"
 # make -j4 "$@"
 
