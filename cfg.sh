5be906065ebbbcaa46d0043c706d46adc653c452
dmn <graahnul.grom@gmail.com>
2021-04-25 14:32:06 +0300

    update config/build helper scripts
    compiler flags should be set in config script now

--- cfg.sh
+++ cfg.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+# debug:
+#
+export CFLAGS="-O0 -ggdb"
+export CPPFLAGS="-DDEBUG"
+
+# release:
+#
+# export CFLAGS="-O2"
+
 prefix=$t/LC
 export PKG_CONFIG_PATH=$lepton/bin.master/lib/pkgconfig
 # export PKG_CONFIG_PATH=$lepton/bin.tb/lib/pkgconfig
 
-./configure --prefix=$prefix
+./configure --prefix=$prefix "$@"
 
