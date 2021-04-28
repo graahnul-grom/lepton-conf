c8944250b3446fefe29a6935665e61f2b81f0377
dmn <graahnul.grom@gmail.com>
2021-04-28 12:58:52 +0300

    shorter bookmarks mgr dialog title

--- src/events.c
+++ src/events.c
@@ -1701,7 +1701,7 @@ void
 on_mitem_bookmark_manage( GtkMenuItem* mitem, gpointer p )
 {
     gchar* str = settings_bookmarks_to_string();
-    gchar* res = attrs_dlg_run( str, "Manage Bookmarks", "bookmarks" );
+    gchar* res = attrs_dlg_run( str, "Bookmarks", "bookmarks" );
     printf( " >>     res: [%s]\n", res );
 
     if ( res != NULL )
