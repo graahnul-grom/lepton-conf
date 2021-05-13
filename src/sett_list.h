ba1c62b74d887cace53a4650819ac42c232da0b7
dmn <graahnul.grom@gmail.com>
2021-05-13 17:16:43 +0300

    add file headers to new files

--- src/sett_list.h
+++ src/sett_list.h
@@ -1,6 +1,14 @@
 #ifndef LEPTON_CONF_SETT_LIST_H_
 #define LEPTON_CONF_SETT_LIST_H_
 
+/*
+ * lepton-conf - Lepton EDA configuration utility.
+ * https://github.com/graahnul-grom/lepton-conf
+ * Copyright (C) 2017-2021 dmn <graahnul.grom@gmail.com>
+ * License: GPL2 - same as Lepton EDA, see
+ * https://github.com/lepton-eda/lepton-eda
+ */
+
 gchar*
 settings_list_to_string( GList* lst );
 
@@ -18,10 +26,6 @@ settings_list_load( GList** lst, const gchar* name );
 
 void
 settings_list_clear( GList** lst );
-
-
-
-
 
 
 #endif // LEPTON_CONF_SETT_LIST_H_
